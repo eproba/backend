1b7b8cc5837c98adfdf3c817ba11b654af8fe4ac
Antoni-Czaplicki <antekczaplicki@gmail.com>
2021-08-27 14:10:26 +0200

    Reformat code

--- scouts_exams/apps/users/migrations/0005_auto_20210827_1246.py
+++ scouts_exams/apps/users/migrations/0005_auto_20210827_1246.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('users', '0004_auto_20210826_2035'),
     ]
@@ -29,6 +28,8 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='scout',
             name='function',
-            field=models.IntegerField(choices=[(0, 'Druh'), (1, 'Podzastępowy'), (2, 'Zastępowy'), (3, 'Przyboczny'), (4, 'Drużynowy'), (5, 'Wyższa funkcja')], default=0),
+            field=models.IntegerField(
+                choices=[(0, 'Druh'), (1, 'Podzastępowy'), (2, 'Zastępowy'), (3, 'Przyboczny'), (4, 'Drużynowy'),
+                         (5, 'Wyższa funkcja')], default=0),
         ),
     ]
