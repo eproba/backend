8cd7ab16cc7a1c84d6fa921558038fa0aef7cd74
Antoni-Czaplicki <antekczaplicki@gmail.com>
2022-01-25 23:04:03 +0100

    Add exams archive

--- scouts_exams/apps/exam/urls.py
+++ scouts_exams/apps/exam/urls.py
@@ -5,17 +5,11 @@ from . import views
 app_name = "exam"
 urlpatterns = [
     path("", views.view_exams, name="exam"),
+    path("archive", views.archive, name="archive"),
     path("create", views.create_exam, name="create_exam"),
     path("manage", views.manage_exams, name="manage_exams"),
-    path("<int:exam_id>/task/submit", views.submit_task, name="submit_task"),
-    path("share/view/<str:hex>", views.view_shared_exams, name="view_shared_exams"),
     path("print/<str:hex>", views.print_exam, name="print_exam"),
-    path(
-        "<int:exam_id>/<int:task_id>/task/unsubmit",
-        views.unsubmit_task,
-        name="unsubmit_task",
-    ),
-    path("<int:exam_id>/tasks/sent", views.sent_tasks, name="sent_tasks"),
+    path("share/view/<str:hex>", views.view_shared_exams, name="view_shared_exams"),
     path("tasks/check", views.check_tasks, name="check_tasks"),
     path(
         "<int:exam_id>/<int:task_id>/task/accept",
@@ -24,6 +18,13 @@ urlpatterns = [
     ),
     path(
         "<int:exam_id>/<int:task_id>/task/refuse", views.refuse_task, name="refuse_task"
+    ),
+    path("<int:exam_id>/tasks/sent", views.sent_tasks, name="sent_tasks"),
+    path("<int:exam_id>/task/submit", views.submit_task, name="submit_task"),
+    path(
+        "<int:exam_id>/<int:task_id>/task/unsubmit",
+        views.unsubmit_task,
+        name="unsubmit_task",
     ),
     path(
         "<int:exam_id>/<int:task_id>/task/force/accept",
